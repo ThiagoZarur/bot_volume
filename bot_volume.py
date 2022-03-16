116d6092d7324c11605016d3b5ee3a873379e3d5
Santiago Flores <thiagotrader@outlook.es>
2022-03-15 22:00:14 -0600

    Versión corregida con emojis, parametros y correción de pares

--- bot_volume.py
+++ bot_volume.py
@@ -1,3 +1,4 @@
+from matplotlib.colors import SymLogNorm
 from telegram import Update
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, dispatcher,CommandHandler
 from binance.client import Client
@@ -14,19 +15,21 @@ TOKEN = config('TOKEN', default='')
 
 emoji_buy = '\U0001F4C8'
 emoji_sell = '\U0001F4C9'
-my_city_timezone = pytz.timezone('America/Mexico_City')
-my_city_time = datetime.now(my_city_timezone)
+emoji_green_circle = '\U0001F7E2'
+emoji_red_circle = '\U0001F534'
 client = Client(API_KEY,API_SECRET)
 
 
-def detector_trans(update:Updater,context:CallbackContext):
+def detector_trans(update:Updater,context: CallbackContext):
+        
         while True: 
-                watchlist = ['BTCUSDT', 'ETHUSDT','MATICUSDT','ADAUSDT','DOTUSDT','XLMUSDT','ZRXUSDT','LINKUSDT','SOLUSDT','MANAUSDT','SUSHIUSDT','UNIUSDT']
+                watchlist = ['ADAUSDT','ADABUSD','ADABTC','ATOMUSDT','AVAXUSDT','BNBUSDT','BTCUSDT','DOTUSDT','ETHUSDT','ETHBTC','ENJUSDT','INJUSDT','LINKUSDT','LUNAUSDT',
+                'MATICUSDT','MATICBTC','MANAUSDT','STXUSDT','SOLUSDT','SUSHIUSDT','UNIUSDT','WAVESUSDT','XLMUSDT','XLMBTC','ZRXUSDT']
                 for ticker in watchlist:
                         pair = ticker
 
                         #Accessing the function of the API-Binance
-                        symbol = client.get_recent_trades(symbol=(pair),limit=1)        
+                        symbol = client.get_recent_trades(symbol=(pair),limit=10)        
                         for trans in symbol:
                                 quantity = float(trans['qty'])
                                 price = float(trans['price'])
@@ -34,31 +37,29 @@ def detector_trans(update:Updater,context:CallbackContext):
                                 total_usd = quantity * price
 
                                 #Filter to the transactional volume 
-                                if total_usd >= 300000 and buy_sell == False:
-                                        update.message.reply_text(f'''Moneda: {pair}
-BIG BUY DETECTED {emoji_buy}
-Cantidad en monedas: {quantity}
-Precio: {price}
-Cantidad en usd: {total_usd}''')
-                                        #Remove variables so as not to accumulate data
-                                        del(pair)
+                                if total_usd >= 50000 and buy_sell == False:
+                                        update.message.reply_text(f'''BIG BUY DETECTED {emoji_buy} {emoji_green_circle} 
+                {pair}
+Cantidad monedas: {quantity}
+Precio: {price}''' '\n'
+'Cantidad usd: ' '{0:,.2f}'.format(total_usd))
                                         
 
-                                elif total_usd >=  300000 and buy_sell == True:
-                                        update.message.reply_text(f'''Moneda: {pair}
-BIG SELL DETECTED {emoji_sell}
+                                elif total_usd >=  50000 and buy_sell == True:
+                                        update.message.reply_text(f'''BIG SELL DETECTED {emoji_sell} {emoji_red_circle}
+                {pair}
 Cantidad en monedas: {quantity}
-Precio: {price}
-Cantidad en usd: {total_usd}''')
-                                        #Remove variables so as not to accumulate data
-                                        del(pair)
+Precio: {price}''' '\n'
+'Cantidad en usd: ' '{0:,.2f}'.format(total_usd))
 
                                 
 def run():
         updater = Updater(TOKEN)
         dispatcher = updater.dispatcher
 
+        
         dispatcher.add_handler(CommandHandler('start',detector_trans))
+
 
         updater.start_polling()
         updater.idle()
